19b961d889301351b81e5bb937881d583801a41e
Billy-06 <ndegwah4billy@gmail.com>
2023-10-09 14:00:23 +0800

    Initial Commit - Setup the dataloader and the default Model structure

---
+++ ModelTraining/Model.py
@@ -0,0 +1,1 @@
+from ModelTraining import Model
---
+++ data/__init__.py
@@ -0,0 +1,6 @@
+# # Download CUB_200_2011 data
+# !wget "https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz"
+
+# # Extract the CUB_200 dataset
+# !tar -xvf CUB_200_2011.tgz
+# !rm -f CUB_200_2011.tgz
